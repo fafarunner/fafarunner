418eaeb2a9a02fc7e26732bc853ca86e9b7bde04
kjxbyz <kjxbyz@163.com>
2023-05-05 23:03:34 +0800

    fix: 异常上报

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -5,10 +5,25 @@ import 'package:fafa_runner/app/app.dart';
 import 'package:flutter/widgets.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
+Future<void> collectLog(String line) async {
+  log(line);
+  await Sentry.captureMessage(line);
+}
+
+Future<void> reportErrorAndLog(FlutterErrorDetails details) async {
+  log(details.exceptionAsString(), stackTrace: details.stack);
+  await Sentry.captureException(details.exception, stackTrace: details.stack);
+}
+
+FlutterErrorDetails makeErrorDetails(Object error, StackTrace stackTrace) {
+  return FlutterErrorDetails(exception: error, stack: stackTrace);
+}
+
 void bootstrap() {
+  final onError = FlutterError.onError;
   FlutterError.onError = (details) async {
-    log(details.exceptionAsString(), stackTrace: details.stack);
-    await Sentry.captureException(details.exception, stackTrace: details.stack);
+    onError?.call(details);
+    await reportErrorAndLog(details);
   };
 
   runZonedGuarded(
@@ -27,8 +42,13 @@ void bootstrap() {
       );
     },
     (error, stackTrace) async {
-      log(error.toString(), stackTrace: stackTrace);
-      await Sentry.captureException(error, stackTrace: stackTrace);
+      await reportErrorAndLog(makeErrorDetails(error, stackTrace));
     },
+    zoneSpecification: ZoneSpecification(
+      print: (Zone self, ZoneDelegate parent, Zone zone, String line) async {
+        await collectLog(line);
+        // parent.print(zone, line);
+      },
+    ),
   );
 }
