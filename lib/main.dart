30b1ba6ddb985c7b9884129a3899feaea09e8d6a
kjxbyz <kjxbyz@163.com>
2023-10-21 16:34:44 +0800

    perf: 代码优化

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'dart:developer';
 import 'package:fafa_runner/app/app.dart';
 import 'package:fafa_runner/constrants/get.dart';
 import 'package:fafa_runner/url_strategy/url_strategy_non_web.dart'
-if (dart.library.html) 'package:fafa_runner/url_strategy/url_strategy_web.dart';
+    if (dart.library.html) 'package:fafa_runner/url_strategy/url_strategy_web.dart';
 import 'package:fafa_runner/util/sounds.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flame/flame.dart';
@@ -28,60 +28,51 @@ FlutterErrorDetails makeErrorDetails(Object error, StackTrace stackTrace) {
   return FlutterErrorDetails(exception: error, stack: stackTrace);
 }
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   final onError = FlutterError.onError;
   FlutterError.onError = (details) async {
     onError?.call(details);
     await reportErrorAndLog(details);
   };
 
-  runZonedGuarded(
-    () async {
-      WidgetsFlutterBinding.ensureInitialized();
+  PlatformDispatcher.instance.onError = (error, stackTrace) {
+    reportErrorAndLog(makeErrorDetails(error, stackTrace));
+    return true;
+  };
 
-      setPathUrlStrategy();
+  setPathUrlStrategy();
 
-      if (!kIsWeb) {
-        await Flame.device.setLandscape();
-        await Flame.device.fullScreen();
-      }
-      await Sounds.initialize();
+  if (!kIsWeb) {
+    await Flame.device.setLandscape();
+    await Flame.device.fullScreen();
+  }
+  await Sounds.initialize();
 
-      if (isMobile) {
-        await Firebase.initializeApp();
-      }
+  if (isMobile) {
+    await Firebase.initializeApp();
+  }
 
-      if (isDesktop) {
-        await WindowManager.instance.ensureInitialized();
-        await windowManager.waitUntilReadyToShow().then((_) async {
-          await windowManager.center();
-          await windowManager.show();
-          await windowManager.setPreventClose(true);
-          await windowManager.setSkipTaskbar(false);
-        });
-      }
-      await SentryFlutter.init(
-        (options) {
-          options
-            ..dsn =
-                'https://c0846b730913410f9041993f54e641ec@o513893.ingest.sentry.io/4505132030296064'
-            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-            // We recommend adjusting this value in production.
-            ..tracesSampleRate = 1.0;
-        },
-      );
-      runApp(
-        const App(),
-      );
+  if (isDesktop) {
+    await WindowManager.instance.ensureInitialized();
+    await windowManager.waitUntilReadyToShow().then((_) async {
+      await windowManager.center();
+      await windowManager.show();
+      await windowManager.setPreventClose(true);
+      await windowManager.setSkipTaskbar(false);
+    });
+  }
+  await SentryFlutter.init(
+    (options) {
+      options
+        ..dsn =
+            'https://c0846b730913410f9041993f54e641ec@o513893.ingest.sentry.io/4505132030296064'
+        // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+        // We recommend adjusting this value in production.
+        ..tracesSampleRate = 1.0;
     },
-    (error, stackTrace) async {
-      await reportErrorAndLog(makeErrorDetails(error, stackTrace));
-    },
-    zoneSpecification: ZoneSpecification(
-      print: (Zone self, ZoneDelegate parent, Zone zone, String line) async {
-        await collectLog(line);
-        // parent.print(zone, line);
-      },
-    ),
   );
+
+  runApp(const App());
 }
