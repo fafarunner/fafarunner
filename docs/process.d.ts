23255488e9aa71fc5e393b70a3aa72c795be6ef9
kjxbyz <kjxbyz888@outlook.com>
2024-02-28 16:45:32 +0800

    fix: hide particles

--- docs/process.d.ts
+++ docs/process.d.ts
@@ -3,6 +3,7 @@ declare namespace NodeJS {
     VERCEL_GIT_COMMIT_SHA: string;
     NEXT_PUBLIC_VERCEL_ENV: string;
     NEXT_PUBLIC_GOOGLE_ID: string;
+    NEXT_PUBLIC_SHOW_PARTICLES: string;
     NEXT_PUBLIC_GH_TOKEN: string;
     NEXT_PUBLIC_GH_REPO_OWNER: string;
     NEXT_PUBLIC_GH_REPO: string;
