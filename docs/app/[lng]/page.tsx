eed3bbd8f26b4a181ffa3576fea26f9f23589375
kjxbyz <kjxbyz@163.com>
2024-04-04 21:15:16 +0800

    refactor: remove ui components

--- docs/app/[lng]/page.tsx
+++ docs/app/[lng]/page.tsx
@@ -3,30 +3,12 @@ import React from "react";
 import Link from "next/link";
 import Balancer from "react-wrap-balancer";
 import { RoughNotation } from "react-rough-notation";
-import { FiMail } from "react-icons/fi";
-import { IoGameControllerOutline } from "react-icons/io5";
-import {
-  SiTaobao,
-  SiSpotify,
-  SiSinaweibo,
-  SiInstagram,
-  SiYoutube,
-  SiTwitch,
-  SiTwitter,
-  SiTelegram,
-} from "react-icons/si";
-import { BiTestTube } from "react-icons/bi";
 import { FaBlog } from "react-icons/fa";
 import Image from "next/image";
-// import dynamic from "next/dynamic";
-import GithubRelease from "@/components/home/github-release";
+import GitHubRelease from "@/components/home/github-release";
 import { useTranslation } from "@/i18n/client";
 import { basePath } from "@/constants";
 import { allPosts } from "contentlayer/generated";
-
-// const DynamicCard = dynamic(() => import("@/components/home/card"), {
-//   ssr: false,
-// });
 
 export default function Home({
   params,
@@ -36,7 +18,6 @@ export default function Home({
   };
 }) {
   const { t } = useTranslation(params.lng, "header");
-  const { t: tc } = useTranslation(params.lng, "common");
 
   const post = allPosts
     .filter((post) => post.slug.startsWith(`${params.lng}/blog`))
@@ -92,148 +73,7 @@ export default function Home({
           </Balancer>
         </p>
       </div>
-      <GithubRelease lng={params.lng} />
-      {/*<div className="mt-10 grid w-full max-w-screen-xl animate-fade-up xl:px-0">*/}
-      {/*  <div className="flex items-center justify-center">*/}
-      {/*    <div className="grid w-full grid-cols-1 gap-5 md:max-w-3xl md:grid-cols-2">*/}
-      {/*      <Link*/}
-      {/*        href="https://www.chenyifaer.com/join"*/}
-      {/*        target="_blank"*/}
-      {/*        className="flex items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800 hover:enabled:border-gray-800 disabled:cursor-not-allowed dark:bg-black dark:text-white/80 max-md:mx-10"*/}
-      {/*        rel="noopener noreferrer"*/}
-      {/*      >*/}
-      {/*        <BiTestTube className="h-7 w-7" />*/}
-      {/*        <p>*/}
-      {/*          <span className="sm:inline-block">{tc("join")}</span>*/}
-      {/*        </p>*/}
-      {/*      </Link>*/}
-      {/*      <Link*/}
-      {/*        href="https://www.chenyifaer.com/fafa-runner/play"*/}
-      {/*        target="_blank"*/}
-      {/*        className="flex items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800 hover:enabled:border-gray-800 disabled:cursor-not-allowed dark:bg-black dark:text-white/80 max-md:mx-10"*/}
-      {/*        rel="noopener noreferrer"*/}
-      {/*      >*/}
-      {/*        <IoGameControllerOutline className="h-7 w-7" />*/}
-      {/*        <p>*/}
-      {/*          <span className="sm:inline-block">{tc("play")}</span>*/}
-      {/*        </p>*/}
-      {/*      </Link>*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
-      {/*<div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 sm:grid-cols-2 lg:grid-cols-3 xl:px-0">*/}
-      {/*  {features.map(({ title, description, demo, url }) => (*/}
-      {/*    <DynamicCard*/}
-      {/*      key={title}*/}
-      {/*      title={title}*/}
-      {/*      description={description}*/}
-      {/*      demo={demo}*/}
-      {/*      url={url}*/}
-      {/*    />*/}
-      {/*  ))}*/}
-      {/*</div>*/}
+      <GitHubRelease lng={params.lng} />
     </>
   );
 }
-
-const features = [
-  {
-    title: "Taobao",
-    description:
-      "Pre-built beautiful, a11y-first components, powered by [Tailwind CSS](https://tailwindcss.com/), [Radix UI](https://www.radix-ui.com/), and [Framer Motion](https://framer.com/motion)",
-    demo: (
-      <SiTaobao className="h-24 w-24 text-gray-600 transition-all dark:text-white/80" />
-    ),
-    url: "https://chenyifaer.taobao.com",
-  },
-  {
-    title: "Instagram",
-    description:
-      "Built on [Next.js](https://nextjs.org/) primitives like `@next/font` and `next/image` for stellar performance.",
-    demo: (
-      <SiInstagram className="h-24 w-24 text-gray-600 transition-all dark:text-white/80" />
-    ),
-    url: "https://instagram.com/yifaer_chen",
-  },
-  {
-    title: "YouTube",
-    description:
-      "Jumpstart your next project by deploying Precedent to [Vercel](https://vercel.com/) in one click.",
-    demo: (
-      <SiYoutube className="h-24 w-24 text-gray-600 transition-all dark:text-white/80" />
-    ),
-    url: "https://www.youtube.com/@chenyifaer",
-  },
-  {
-    title: "Twitch",
-    description:
-      "Jumpstart your next project by deploying Precedent to [Vercel](https://vercel.com/) in one click.",
-    demo: (
-      <SiTwitch className="h-24 w-24 text-gray-600 transition-all dark:text-white/80" />
-    ),
-    url: "https://www.twitch.tv/thebs_chen",
-  },
-  {
-    title: "Spotify",
-    description:
-      "Jumpstart your next project by deploying Precedent to [Vercel](https://vercel.com/) in one click.",
-    demo: (
-      <SiSpotify className="h-24 w-24 text-gray-600 transition-all dark:text-white/80" />
-    ),
-    url: "https://open.spotify.com/artist/10xtjTRMlKZ7aFx6VBQlSj",
-  },
-  {
-    title: "Weibo",
-    description:
-      "Jumpstart your next project by deploying Precedent to [Vercel](https://vercel.com/) in one click.",
-    demo: (
-      <SiSinaweibo className="h-24 w-24 text-gray-600 transition-all dark:text-white/80" />
-    ),
-    url: "https://weibo.com/u/7357828611",
-  },
-  {
-    title: "Twitter",
-    description:
-      "Jumpstart your next project by deploying Precedent to [Vercel](https://vercel.com/) in one click.",
-    demo: (
-      <SiTwitter className="h-24 w-24 text-gray-600 transition-all dark:text-white/80" />
-    ),
-    url: "https://twitter.com/yifaer_chen",
-  },
-  {
-    title: "Telegram",
-    description:
-      "Jumpstart your next project by deploying Precedent to [Vercel](https://vercel.com/) in one click.",
-    demo: (
-      <SiTelegram className="h-24 w-24 text-gray-600 transition-all dark:text-white/80" />
-    ),
-    url: "https://t.me/FaFa67373",
-  },
-  {
-    title: "Email",
-    description:
-      "Precedent comes with authentication and database via [Auth.js](https://authjs.dev/) + [Prisma](https://prisma.io/)",
-    demo: (
-      <FiMail className="h-24 w-24 text-gray-600 transition-all dark:text-white/80" />
-    ),
-    url: "mailto:chenyifaer777@gmail.com",
-  },
-  {
-    title: "FaFa Runner",
-    description:
-      "Precedent offers a collection of hooks, utilities, and `@vercel/og`",
-    demo: (
-      <IoGameControllerOutline className="h-24 w-24 text-gray-600 transition-all dark:text-white/80" />
-    ),
-    url: "https://chenyifaer.com/fafa-runner",
-  },
-  {
-    title: "CYF Insider",
-    description:
-      "Precedent offers a collection of hooks, utilities, and `@vercel/og`",
-    demo: (
-      <BiTestTube className="h-24 w-24 text-gray-600 transition-all dark:text-white/80" />
-    ),
-    url: "https://chenyifaer.com/join",
-  },
-];
