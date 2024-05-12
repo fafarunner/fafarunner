5e12df874f0f4a89e9ce0b1030b107805f36ec0e
kjxbyz <kjxbyz888@outlook.com>
2024-05-12 20:39:30 +0800

    build: update build-web.sh

--- build-web.sh
+++ build-web.sh
@@ -4,10 +4,10 @@ set -e
 
 if [[ "$VERCEL_ENV" == "preview" ]] ; then
   echo "🔥Building web app in preview environment"
-  flutter/bin/flutter build web --no-tree-shake-icons --web-renderer canvaskit;
+  flutter/bin/flutter build web --no-tree-shake-icons --web-renderer canvaskit
 else
   echo "🔥Building web app in production environment"
-  flutter/bin/flutter build web --no-tree-shake-icons --web-renderer canvaskit --base-href /fafa-runner/play/;
+  flutter/bin/flutter build web --no-tree-shake-icons --web-renderer canvaskit
 fi
 
 exit 0
