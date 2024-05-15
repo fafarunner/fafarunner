2be5daa414159513778ad3f0f6ea0129ef59aba2
kjxbyz <kjxbyz@163.com>
2024-05-15 15:31:47 +0800

    fix: update build-web.sh

--- build-web.sh
+++ build-web.sh
@@ -4,10 +4,10 @@ set -e
 
 if [[ "$VERCEL_ENV" == "preview" ]] ; then
   echo "🔥Building web app in preview environment"
-  flutter/bin/flutter build web --no-tree-shake-icons --web-renderer canvaskit
+  flutter/bin/flutter build web
 else
   echo "🔥Building web app in production environment"
-  flutter/bin/flutter build web --no-tree-shake-icons --web-renderer canvaskit
+  flutter/bin/flutter build web
 fi
 
 exit 0
