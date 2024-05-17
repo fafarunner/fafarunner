91bf835618a854a33a2c4357ba80c190af46e39b
kjxbyz <kjxbyz@163.com>
2024-05-17 16:36:25 +0800

    fix: update actions
    [skip ci]

--- build-web.sh
+++ build-web.sh
@@ -7,7 +7,9 @@ if [[ "$VERCEL_ENV" == "preview" ]] ; then
   flutter/bin/flutter build web --dart-define SENTRY_DSN="$FR_ENV_STG"
 else
   echo "🔥Building web app in production environment"
-  COMMIT_COUNT="$(git rev-list --count main)"
+  echo "VERCEL_GIT_COMMIT_SHA: $VERCEL_GIT_COMMIT_SHA"
+  COMMIT_COUNT="$(git rev-list --count $VERCEL_GIT_COMMIT_SHA)"
+  echo "COMMIT_COUNT: $COMMIT_COUNT"
   flutter/bin/flutter build web --dart-define SENTRY_DSN="$FR_ENV_PROD" --build-number "$COMMIT_COUNT"
 fi
 
