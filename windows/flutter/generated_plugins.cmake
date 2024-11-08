00bac8545559e67cc4b2c3710b0bd6c8c8db4a53
kjxbyz <kjxbyz888@outlook.com>
2024-11-09 01:54:07 +0800

    Update sentry to 8.10.1

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,11 +4,11 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   audioplayers_windows
+  sentry_flutter
   url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
-  sentry_flutter
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
