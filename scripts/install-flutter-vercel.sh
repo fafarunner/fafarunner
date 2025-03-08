e41e6eb809addf5efb2114e864b17e57b68f449f
kjxbyz <kjxbyz888@outlook.com>
2025-03-08 19:53:35 +0800

    Update flutter to v3.29.1

--- scripts/install-flutter-vercel.sh
+++ scripts/install-flutter-vercel.sh
@@ -9,9 +9,9 @@ fi
 
 FLUTTER_DIR="$PWD"
 
-if [ ! -f "$FLUTTER_DIR/flutter_linux_3.29.0-stable.tar.xz" ]; then
+if [ ! -f "$FLUTTER_DIR/flutter_linux_3.29.1-stable.tar.xz" ]; then
    echo "🟩 Downloading Flutter"
-   wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.29.0-stable.tar.xz -P "$FLUTTER_DIR"
+   wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.29.1-stable.tar.xz -P "$FLUTTER_DIR"
 fi
 
 if [ -d "$FLUTTER_DIR/flutter" ]; then
@@ -20,10 +20,14 @@ if [ -d "$FLUTTER_DIR/flutter" ]; then
 fi
 
 echo "🟩 Unzipping"
-mkdir "$FLUTTER_DIR/flutter" && tar -xf "$FLUTTER_DIR/flutter_linux_3.29.0-stable.tar.xz" -C "$FLUTTER_DIR/flutter" --strip-components 1
+mkdir "$FLUTTER_DIR/flutter" && tar -xf "$FLUTTER_DIR/flutter_linux_3.29.1-stable.tar.xz" -C "$FLUTTER_DIR/flutter" --strip-components 1
 
 echo "🟩 Config $FLUTTER_DIR/flutter"
 git config --global --add safe.directory "$FLUTTER_DIR/flutter"
+
+# https://github.com/canonical/snapcraft/pull/5264
+# Fixed in 3.29.1
+#rm -f "$FLUTTER_DIR/flutter/engine/src/.gn"
 
 echo "🟩 Running ls"
 ls
