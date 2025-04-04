993c85a2a7130e0579dcf1b5ec8211a41f1e6979
kjxbyz <kjxbyz@users.noreply.github.com>
2025-04-04 07:31:13 +0000

    deploy: 2b28b45a27ba1491f58bdad4773926f504178453

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -2,100 +2,176 @@
 a[b][c]=a[b][c]||[]
 a[b][c].push({p:"main.dart.js_1",e:"beginPart"})})(self,"$__dart_deferred_initializers__","eventLog")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var B,C,A={
-aL7(d,e,f){var w,v,u=x.c,t=new B.m3(C.lo,u)
-u=new A.Tr(t,new B.m3(C.eI,u))
-u.Ma(d,e,null)
-w=u.a
+aRl(d,e,f){var w,v,u=null,t=new B.mE(C.ny,x.c),s=new B.mE(C.fi,x.c)
+s=new A.VH(t,s)
+s.NH(d,u,e,u)
+w=s.a
 v=t.r
 v.toString
 w.r=v
-t.r=A.b2a(u)
-return u},
-b29(d,e){switch(e){case"pages.appName":return"\u53d1\u53d1\u8dd1\u9177"
-case"pages.playCap":return"\u5f00\u59cb\u73a9"
-case"pages.loading":return"\u52a0\u8f7d\u4e2d..."
-case"pages.creditsCap":return"\u5b66\u5206"
-case"pages.poweredByPrefix":return"\u7531 "
-case"pages.poweredBySuffix":return" \u5f00\u53d1"
-case"pages.author":return"\u79d1\u6280\u5c0f\u767d\u82f1\u4ed4"
-case"pages.version":return new A.azg()
-case"pages.talkWizard1":return"\u4f60\u597d, \u6211\u5e74\u8f7b\u7684\u9a91\u58eb! \n\u4f60\u5728\u8fd9\u91cc\u505a\u4ec0\u4e48?"
-case"pages.talkPlayer1":return"\u4f60\u597d! \n\u6211\u88ab\u6d3e\u53bb\u8425\u6551\u4e00\u4e2a\u88ab\u751f\u6d3b\u5728\u8fd9\u4e9b\u8857\u533a\u7684\u751f\u7269\u7ed1\u67b6\u7684\u5b69\u5b50."
-case"pages.talkWizard2":return y.m
-case"pages.talkPlayer2":return"\u522b\u62c5\u5fc3\u6211\u7684\u8001\u4eba. \u4ece\u6765\u6ca1\u6709\u50cf\u6211\u8fd9\u6837\u7684\u9a91\u58eb!\n\u6211\u8981\u6d88\u706d\u8fd9\u91cc\u7684\u4e00\u5207\u751f\u7075, \u6551\u51fa\u5b69\u5b50!"
-case"pages.talkWizard3":return"\u662f\u7684, \u81ea\u4fe1\u662f\u4f60\u6700\u4e0d\u7f3a\u7684!\n\u90a3\u5c31\u522b\u8bf4\u6211\u6ca1\u63d0\u9192\u4f60!\n\u795d\u4f60\u597d\u8fd0!"
-case"pages.talkKid1":return"\u6551\u547d! \u6551\u547d!"
-case"pages.talkBoss1":return"\u95ed\u5634, \u4f60\u8fd9\u4e2a\u4ee4\u4eba\u5782\u6d8e\u6b32\u6ef4\u7684\u5c0f\u4e1c\u897f!\n\u662f\u65f6\u5019\u54c1\u5c1d\u4f60\u67d4\u8f6f\u7684\u76ae\u80a4\u4e86! \u54c8\u54c8\u54c8..."
-case"pages.talkPlayer3":return"\u8fd9\u6b21\u5b83\u4e0d\u4f1a\u662f\u4f60\u8ba8\u538c\u7684\u751f\u7269!\n\u4f60\u53ef\u6015\u7684\u65e5\u5b50\u7ed3\u675f\u4e86!"
-case"pages.talkBoss2":return"\u770b\u90a3\u4e2a! \u53e6\u4e00\u4e2a\u81ea\u8d1f\u7684\u9a91\u58eb\u8ba4\u4e3a\u4ed6\u53ef\u4ee5\u963b\u6b62\u6211! \u54c8\u54c8\u54c8\n\u8fc7\u6765!"
-case"pages.talkKid2":return y.l
-case"pages.talkPlayer4":return y.k
-case"pages.playAgainCap":return"\u518d\u73a9\u4e00\u6b21"
-case"pages.congratulations":return"\u606d\u559c!"
-case"pages.thanks":return y.a
-case"pages.doorWithoutKey":return"\u6211\u60f3\u6211\u9700\u8981\u4e00\u628a\u94a5\u5319\u624d\u80fd\u901a\u8fc7\u8fd9\u91cc!"
+t.r=A.b98(s)
+return s},
+b97(d,e){switch(e){case"appName":return"\u53d1\u53d1\u8dd1\u9177"
+case"menuPage.playCap":return"\u5f00\u59cb\u73a9"
+case"menuPage.keyboard":return"\u952e\u76d8"
+case"menuPage.joystick":return"\u64cd\u7eb5\u6746"
+case"menuPage.loading":return"\u52a0\u8f7d\u4e2d..."
+case"menuPage.creditsCap":return"\u5b66\u5206"
+case"menuPage.poweredByPrefix":return"\u7531 "
+case"menuPage.poweredBySuffix":return" \u5f00\u53d1"
+case"menuPage.author":return"\u79d1\u6280\u5c0f\u767d\u82f1\u4ed4"
+case"menuPage.version":return new A.aEV()
+case"gamePage.talkWizard1":return"\u4f60\u597d, \u6211\u5e74\u8f7b\u7684\u9a91\u58eb! \n\u4f60\u5728\u8fd9\u91cc\u505a\u4ec0\u4e48?"
+case"gamePage.talkPlayer1":return"\u4f60\u597d! \n\u6211\u88ab\u6d3e\u53bb\u8425\u6551\u4e00\u4e2a\u88ab\u751f\u6d3b\u5728\u8fd9\u4e9b\u8857\u533a\u7684\u751f\u7269\u7ed1\u67b6\u7684\u5b69\u5b50."
+case"gamePage.talkWizard2":return y.m
+case"gamePage.talkPlayer2":return"\u522b\u62c5\u5fc3\u6211\u7684\u8001\u4eba. \u4ece\u6765\u6ca1\u6709\u50cf\u6211\u8fd9\u6837\u7684\u9a91\u58eb!\n\u6211\u8981\u6d88\u706d\u8fd9\u91cc\u7684\u4e00\u5207\u751f\u7075, \u6551\u51fa\u5b69\u5b50!"
+case"gamePage.talkWizard3":return"\u662f\u7684, \u81ea\u4fe1\u662f\u4f60\u6700\u4e0d\u7f3a\u7684!\n\u90a3\u5c31\u522b\u8bf4\u6211\u6ca1\u63d0\u9192\u4f60!\n\u795d\u4f60\u597d\u8fd0!"
+case"gamePage.talkKid1":return"\u6551\u547d! \u6551\u547d!"
+case"gamePage.talkBoss1":return"\u95ed\u5634, \u4f60\u8fd9\u4e2a\u4ee4\u4eba\u5782\u6d8e\u6b32\u6ef4\u7684\u5c0f\u4e1c\u897f!\n\u662f\u65f6\u5019\u54c1\u5c1d\u4f60\u67d4\u8f6f\u7684\u76ae\u80a4\u4e86! \u54c8\u54c8\u54c8..."
+case"gamePage.talkPlayer3":return"\u8fd9\u6b21\u5b83\u4e0d\u4f1a\u662f\u4f60\u8ba8\u538c\u7684\u751f\u7269!\n\u4f60\u53ef\u6015\u7684\u65e5\u5b50\u7ed3\u675f\u4e86!"
+case"gamePage.talkBoss2":return"\u770b\u90a3\u4e2a! \u53e6\u4e00\u4e2a\u81ea\u8d1f\u7684\u9a91\u58eb\u8ba4\u4e3a\u4ed6\u53ef\u4ee5\u963b\u6b62\u6211! \u54c8\u54c8\u54c8\n\u8fc7\u6765!"
+case"gamePage.talkKid2":return y.l
+case"gamePage.talkPlayer4":return y.k
+case"gamePage.playAgainCap":return"\u518d\u73a9\u4e00\u6b21"
+case"gamePage.congratulations":return"\u606d\u559c!"
+case"gamePage.thanks":return y.a
+case"gamePage.doorWithoutKey":return"\u6211\u60f3\u6211\u9700\u8981\u4e00\u628a\u94a5\u5319\u624d\u80fd\u901a\u8fc7\u8fd9\u91cc!"
+case"hotkeyDialog.description":return"\u8bf7\u70b9\u51fb\u952e\u76d8\u4e0a\u7684\u952e\u6765\u8bbe\u7f6e\u5feb\u6377\u952e"
+case"tray.settings":return"\u8bbe\u7f6e"
+case"tray.exit":return"\u9000\u51fa"
+case"buttons.cancel":return"\u53d6\u6d88"
+case"buttons.ok":return"\u786e\u5b9a"
+case"settings.shortcutsTitle":return"\u5feb\u6377\u952e"
+case"settings.shortcutsDescription":return"\u8b66\u544a: \u6682\u4e0d\u652f\u6301\u5b9a\u4e49\u7ec4\u5408\u952e"
+case"settings.shortcutsRegistered":return"\u8be5\u952e\u662f\u5e94\u7528\u5185\u7f6e\u952e"
+case"settings.shortcutsArrow":return"\u8be5\u952e\u5df2\u4f5c\u4e3a\u65b9\u5411\u952e\u4f7f\u7528"
+case"settings.shortcutsUsed":return"\u8be5\u952e\u5df2\u4f7f\u7528"
+case"settings.shortcuts.move":return"\u79fb\u52a8"
+case"settings.shortcuts.attack":return"\u653b\u51fb"
+case"settings.shortcuts.fire":return"\u5f00\u706b"
 case"locales.en":return"English"
 case"locales.zh":return"\u4e2d\u6587"
 default:return null}},
-b2a(d){return new A.azh(d)},
-Tr:function Tr(d,e){var _=this
-_.d=d
-_.f=_.e=$
+b98(d){return new A.aEW(d)},
+VH:function VH(d,e){var _=this
+_.x=d
+_.ay=_.ax=_.at=_.as=_.Q=_.z=_.y=$
 _.a=e
-_.c=_.b=$},
-ayg:function ayg(){},
-azh:function azh(d){this.a=d},
-azg:function azg(){}}
+_.w=_.r=_.f=_.e=_.d=_.c=_.b=$},
+aDS:function aDS(){},
+aDQ:function aDQ(){},
+aDR:function aDR(){},
+aDV:function aDV(){},
+aDP:function aDP(){},
+aDU:function aDU(d,e){var _=this
+_.c=d
+_.d=$
+_.a=e
+_.b=$},
+aDT:function aDT(){},
+aEW:function aEW(d){this.a=d},
+aEV:function aEV(){}}
 B=c[0]
 C=c[2]
 A=a.updateHolder(c[3],A)
-A.Tr.prototype={
-i(d,e){var w=this.d.BW(e)
-return w==null?this.a.BW(e):w},
-gcY(){var w,v=this,u=v.f
-if(u===$){w=v.e
-if(w===$){w!==$&&B.aH()
-v.e=v}u!==$&&B.aH()
-u=v.f=new A.ayg()}return u},
-grK(){return this.d}}
-A.ayg.prototype={
-gun(d){return"\u53d1\u53d1\u8dd1\u9177"},
-gWI(){return"\u5f00\u59cb\u73a9"},
-gVZ(){return"\u52a0\u8f7d\u4e2d..."},
-gWN(){return"\u7531 "},
-gWO(){return" \u5f00\u53d1"},
-gSB(){return"\u79d1\u6280\u5c0f\u767d\u82f1\u4ed4"},
-Yd(d,e){return"\u7248\u672c\u53f7: "+e},
-gXO(){return"\u4f60\u597d, \u6211\u5e74\u8f7b\u7684\u9a91\u58eb! \n\u4f60\u5728\u8fd9\u91cc\u505a\u4ec0\u4e48?"},
-gXK(){return"\u4f60\u597d! \n\u6211\u88ab\u6d3e\u53bb\u8425\u6551\u4e00\u4e2a\u88ab\u751f\u6d3b\u5728\u8fd9\u4e9b\u8857\u533a\u7684\u751f\u7269\u7ed1\u67b6\u7684\u5b69\u5b50."},
-gXP(){return y.m},
-gXL(){return"\u522b\u62c5\u5fc3\u6211\u7684\u8001\u4eba. \u4ece\u6765\u6ca1\u6709\u50cf\u6211\u8fd9\u6837\u7684\u9a91\u58eb!\n\u6211\u8981\u6d88\u706d\u8fd9\u91cc\u7684\u4e00\u5207\u751f\u7075, \u6551\u51fa\u5b69\u5b50!"},
-gXQ(){return"\u662f\u7684, \u81ea\u4fe1\u662f\u4f60\u6700\u4e0d\u7f3a\u7684!\n\u90a3\u5c31\u522b\u8bf4\u6211\u6ca1\u63d0\u9192\u4f60!\n\u795d\u4f60\u597d\u8fd0!"},
-gXI(){return"\u6551\u547d! \u6551\u547d!"},
-gXG(){return"\u95ed\u5634, \u4f60\u8fd9\u4e2a\u4ee4\u4eba\u5782\u6d8e\u6b32\u6ef4\u7684\u5c0f\u4e1c\u897f!\n\u662f\u65f6\u5019\u54c1\u5c1d\u4f60\u67d4\u8f6f\u7684\u76ae\u80a4\u4e86! \u54c8\u54c8\u54c8..."},
-gXM(){return"\u8fd9\u6b21\u5b83\u4e0d\u4f1a\u662f\u4f60\u8ba8\u538c\u7684\u751f\u7269!\n\u4f60\u53ef\u6015\u7684\u65e5\u5b50\u7ed3\u675f\u4e86!"},
-gXH(){return"\u770b\u90a3\u4e2a! \u53e6\u4e00\u4e2a\u81ea\u8d1f\u7684\u9a91\u58eb\u8ba4\u4e3a\u4ed6\u53ef\u4ee5\u963b\u6b62\u6211! \u54c8\u54c8\u54c8\n\u8fc7\u6765!"},
-gXJ(){return y.l},
-gXN(){return y.k},
-gWH(){return"\u518d\u73a9\u4e00\u6b21"},
-gT6(){return"\u606d\u559c!"},
-gXR(){return y.a},
-gU1(){return"\u6211\u60f3\u6211\u9700\u8981\u4e00\u628a\u94a5\u5319\u624d\u80fd\u901a\u8fc7\u8fd9\u91cc!"}}
+A.VH.prototype={
+i(d,e){var w=this.x.Dg(e)
+return w==null?this.a.Dg(e):w},
+gqG(){var w=this.y
+if(w===$){w!==$&&B.au()
+w=this.y=this}return w},
+gvl(d){return"\u53d1\u53d1\u8dd1\u9177"},
+gm_(){var w=this,v=w.z
+if(v===$){w.gqG()
+w.z!==$&&B.au()
+v=w.z=new A.aDS()}return v},
+gfC(){var w=this,v=w.Q
+if(v===$){w.gqG()
+w.Q!==$&&B.au()
+v=w.Q=new A.aDQ()}return v},
+gXl(){var w=this,v=w.as
+if(v===$){w.gqG()
+w.as!==$&&B.au()
+v=w.as=new A.aDR()}return v},
+gxp(){var w=this,v=w.at
+if(v===$){w.gqG()
+w.at!==$&&B.au()
+v=w.at=new A.aDV()}return v},
+gea(d){var w=this,v=w.ax
+if(v===$){w.gqG()
+w.ax!==$&&B.au()
+v=w.ax=new A.aDP()}return v},
+gfF(){var w,v=this,u=v.ay
+if(u===$){w=v.gqG()
+v.ay!==$&&B.au()
+u=v.ay=new A.aDU(w,w)}return u},
+gtA(){return this.x}}
+A.aDS.prototype={
+gYG(){return"\u5f00\u59cb\u73a9"},
+gXV(){return"\u952e\u76d8"},
+gXU(){return"\u64cd\u7eb5\u6746"},
+gXZ(){return"\u52a0\u8f7d\u4e2d..."},
+gYK(){return"\u7531 "},
+gYL(){return" \u5f00\u53d1"},
+gUq(){return"\u79d1\u6280\u5c0f\u767d\u82f1\u4ed4"},
+a_d(d,e){return"\u7248\u672c\u53f7: "+e}}
+A.aDQ.prototype={
+gZN(){return"\u4f60\u597d, \u6211\u5e74\u8f7b\u7684\u9a91\u58eb! \n\u4f60\u5728\u8fd9\u91cc\u505a\u4ec0\u4e48?"},
+gZJ(){return"\u4f60\u597d! \n\u6211\u88ab\u6d3e\u53bb\u8425\u6551\u4e00\u4e2a\u88ab\u751f\u6d3b\u5728\u8fd9\u4e9b\u8857\u533a\u7684\u751f\u7269\u7ed1\u67b6\u7684\u5b69\u5b50."},
+gZO(){return y.m},
+gZK(){return"\u522b\u62c5\u5fc3\u6211\u7684\u8001\u4eba. \u4ece\u6765\u6ca1\u6709\u50cf\u6211\u8fd9\u6837\u7684\u9a91\u58eb!\n\u6211\u8981\u6d88\u706d\u8fd9\u91cc\u7684\u4e00\u5207\u751f\u7075, \u6551\u51fa\u5b69\u5b50!"},
+gZP(){return"\u662f\u7684, \u81ea\u4fe1\u662f\u4f60\u6700\u4e0d\u7f3a\u7684!\n\u90a3\u5c31\u522b\u8bf4\u6211\u6ca1\u63d0\u9192\u4f60!\n\u795d\u4f60\u597d\u8fd0!"},
+gZH(){return"\u6551\u547d! \u6551\u547d!"},
+gZF(){return"\u95ed\u5634, \u4f60\u8fd9\u4e2a\u4ee4\u4eba\u5782\u6d8e\u6b32\u6ef4\u7684\u5c0f\u4e1c\u897f!\n\u662f\u65f6\u5019\u54c1\u5c1d\u4f60\u67d4\u8f6f\u7684\u76ae\u80a4\u4e86! \u54c8\u54c8\u54c8..."},
+gZL(){return"\u8fd9\u6b21\u5b83\u4e0d\u4f1a\u662f\u4f60\u8ba8\u538c\u7684\u751f\u7269!\n\u4f60\u53ef\u6015\u7684\u65e5\u5b50\u7ed3\u675f\u4e86!"},
+gZG(){return"\u770b\u90a3\u4e2a! \u53e6\u4e00\u4e2a\u81ea\u8d1f\u7684\u9a91\u58eb\u8ba4\u4e3a\u4ed6\u53ef\u4ee5\u963b\u6b62\u6211! \u54c8\u54c8\u54c8\n\u8fc7\u6765!"},
+gZI(){return y.l},
+gZM(){return y.k},
+gYF(){return"\u518d\u73a9\u4e00\u6b21"},
+gUY(){return"\u606d\u559c!"},
+gZQ(){return y.a},
+gVW(){return"\u6211\u60f3\u6211\u9700\u8981\u4e00\u628a\u94a5\u5319\u624d\u80fd\u901a\u8fc7\u8fd9\u91cc!"}}
+A.aDR.prototype={
+gVF(d){return"\u8bf7\u70b9\u51fb\u952e\u76d8\u4e0a\u7684\u952e\u6765\u8bbe\u7f6e\u5feb\u6377\u952e"}}
+A.aDV.prototype={
+gfF(){return"\u8bbe\u7f6e"},
+gIk(){return"\u9000\u51fa"}}
+A.aDP.prototype={
+gUG(d){return"\u53d6\u6d88"},
+gYn(){return"\u786e\u5b9a"}}
+A.aDU.prototype={
+gMu(){return"\u5feb\u6377\u952e"},
+gMt(){return"\u8b66\u544a: \u6682\u4e0d\u652f\u6301\u5b9a\u4e49\u7ec4\u5408\u952e"},
+gMs(){return"\u8be5\u952e\u5df2\u4f5c\u4e3a\u65b9\u5411\u952e\u4f7f\u7528"},
+gDs(){return"\u8be5\u952e\u5df2\u4f7f\u7528"},
+gfG(){var w=this.d
+if(w===$){w!==$&&B.au()
+w=this.d=new A.aDT()}return w}}
+A.aDT.prototype={
+gJV(){return"\u79fb\u52a8"},
+gHc(d){return"\u653b\u51fb"},
+gIS(){return"\u5f00\u706b"}}
 var z=a.updateTypes([])
-A.azh.prototype={
-$1(d){return A.b29(this.a,d)},
-$S:76}
-A.azg.prototype={
-$1$version(d){return"\u7248\u672c\u53f7: "+B.k(d)},
+A.aEW.prototype={
+$1(d){return A.b97(this.a,d)},
+$S:87}
+A.aEV.prototype={
+$1$version(d){return"\u7248\u672c\u53f7: "+B.l(d)},
 $C:"$1$version",
 $R:0,
-$D(){return{version:C.eW}},
-$S:173};(function inheritance(){var w=a.inherit,v=a.inheritMany
-w(A.Tr,B.fM)
-w(A.ayg,B.Tq)
-v(B.l4,[A.azh,A.azg])})()
-B.aM1(b.typeUniverse,JSON.parse('{"Tr":{"fM":[],"iV":["fp","fM"]}}'))
+$D(){return{version:C.fx}},
+$S:202};(function inheritance(){var w=a.inherit,v=a.inheritMany
+w(A.VH,B.h6)
+w(A.aDS,B.VD)
+w(A.aDQ,B.VB)
+w(A.aDR,B.VC)
+w(A.aDV,B.VG)
+w(A.aDP,B.VA)
+w(A.aDU,B.VE)
+w(A.aDT,B.VF)
+v(B.lF,[A.aEW,A.aEV])})()
+B.aSi(b.typeUniverse,JSON.parse('{"VH":{"h6":[],"je":["fF","h6"]}}'))
 var y={m:"\u55ef\u2026\u2026\n\u6211\u4e0d\u60f3\u6253\u51fb\u4f60, \u4f46\u4f60\u662f\u7b2c\u4e94\u4e2a\u88ab\u6d3e\u6765\u6267\u884c\u8fd9\u9879\u4efb\u52a1\u7684\u9a91\u58eb. \u5230\u76ee\u524d\u4e3a\u6b62, \u6ca1\u6709\u4eba\u6d3b\u7740\u56de\u6765, \u4ed6\u4eec\u7684\u5c38\u4f53\u50cf\u6218\u5229\u54c1\u4e00\u6837\u6302\u5728\u5899\u4e0a.",l:"\u611f\u8c22\u5927\u795e!!! \n\u4f60\u6210\u529f\u6253\u8d25\u4e86\u8fd9\u4e2a\u53ef\u6015\u7684\u751f\u7269! \u975e\u5e38\u611f\u8c22! \n\u6211\u4ec0\u81f3\u4e0d\u77e5\u9053\u5982\u4f55\u611f\u8c22\u4f60!",a:"\u611f\u8c22\u60a8\u6d4b\u8bd5\u6e38\u620f\u5e76\u611f\u53d7 Flame \u548c Bonfire \u5728\u6784\u5efa 2D \u6e38\u620f\u4e2d\u7684\u5a01\u529b.\n\u4e5f\u8bb8\u6211\u4eec\u53ef\u4ee5\u6709\u4e00\u4e2a\u6e38\u620f\u7684\u5ef6\u7eed!\n\u6211\u5e0c\u671b\u4f60\u559c\u6b22!",k:"\u80fd\u4e3a\u60a8\u63d0\u4f9b\u5e2e\u52a9\u662f\u6211\u7684\u8363\u5e78! \u4e5f\u522b\u7740\u6025\u8d4f\u6211, \u4f60\u7239\u53ef\u662f\u7b54\u5e94\u6211\u4e00\u5927\u7b14\u94b1\u6765\u6551\u4f60\u7684! :-)"}
-var x={c:B.an("m3<fp,fM>")}};
+var x={c:B.am("mE<fF,h6>")}};
 ((a,b)=>{a[b]=a.current
-a.eventLog.push({p:"main.dart.js_1",e:"endPart",h:b})})($__dart_deferred_initializers__,"jvykfr98txtIFV1pWGLSeaRRTLQ=");
+a.eventLog.push({p:"main.dart.js_1",e:"endPart",h:b})})($__dart_deferred_initializers__,"4l19OQ62TrUWAl+/h6rmivlpVj4=");
